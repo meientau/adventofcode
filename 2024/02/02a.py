721aaae79e6b755e2cf0b8d0d632024435a1ba46
Jan Schormann <jan.schormann@brainlab.com>
2024-12-02 09:01:05 +0100

    2024-02a using sets as prep for b

--- 2024/02/02a.py
+++ 2024/02/02a.py
@@ -1,11 +1,16 @@
 from fileinput import input
 
+safes = [{-1, -2, -3}, {1, 2, 3}]
+
 count = 0
 for line in input():
     line = [int(v) for v in line.split()]
-    steps = { b-a for a, b in zip(line, line[1:]) }
-    print(steps)
-    if min(steps) >= -3 and max(steps) <= -1 or min(steps) >= 1 and max(steps) <= 3: 
+    found = { b-a for a, b in zip(line, line[1:]) }
+    safesteps = [found-safe for safe in safes]
+    print(f"{found=} {safesteps=}")
+    if not safesteps[0] or not safesteps[1]:
         count += 1
-        
+
 print(count)
+if count in {2, 660}:
+    print("OK")

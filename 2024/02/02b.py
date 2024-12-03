83ad4de7745267c2d0a2ddd87b0babff756ef918
Jan Schormann <jan.schormann@brainlab.com>
2024-12-03 07:33:51 +0100

    2024-02b

--- 2024/02/02b.py
+++ 2024/02/02b.py
@@ -1,5 +1,29 @@
 from fileinput import input
+from itertools import pairwise
+
+safes = [{-1, -2, -3}, {1, 2, 3}]
+
+def is_safe(numbers):
+    found = { b-a for a, b in pairwise(numbers) }
+    safesteps = [found-safe for safe in safes]
+    print(f"{numbers=} {found=} {safesteps=}")
+    return not safesteps[0] or not safesteps[1]
+
+def is_safe_enough(numbers):
+    print(f"{numbers=}")
+    if is_safe(numbers):
+        return True
+
+    for i in range(len(numbers)):
+        clone = list(numbers)
+        del clone[i]
+        if is_safe(clone):
+            return True
+
+    return False
 
 count = 0
 for line in input():
-    pass
+    count += is_safe_enough([int(v) for v in line.split()])
+
+print(count)

c8225998fb8c1fc61eca1cc0048a487abd3ad248
Jan Schormann <jan.schormann@rechengil.de>
2024-12-04 07:47:44 +0100

    2024/04a works

--- 2024/04/04a.py
+++ 2024/04/04a.py
@@ -1,13 +1,42 @@
 from fileinput import input
 from collections import namedtuple
+from pprint import pprint
+
+Point = namedtuple("Point", ["u", "v"])
 
 word = "XMAS"
+directions = [
+    Point( 1,  0),
+    Point( 1,  1),
+    Point( 0,  1),
+    Point(-1,  1),
+    Point(-1,  0),
+    Point(-1, -1),
+    Point( 0, -1),
+    Point( 1, -1),
+]
+def plus(point, direction):
+    return Point(point.u + direction.u, point.v + direction.v)
+
 locations = dict((char, list()) for char in word)
-Point = namedtuple("Point", ["u", "v"])
 
 for v, line in enumerate(input()):
     line = line.strip()
     for u, char in enumerate(line):
         locations[char].append(Point(u, v))
 
-print(locations)
+if len(locations['X']) < 100:
+    pprint(locations)
+
+todo = [ (p, d) for p in locations[word[0]] for d in directions ]
+for char in word[1:]:
+    nexttodo = list()
+    for point, direction in todo:
+        nextpoint = plus(point, direction)
+        nexttodo += [(p, direction) for p in locations[char] if p == nextpoint]
+    todo = nexttodo
+
+print(len(todo))
+
+# small n=18 t=0.02s
+# full  n=2554 t=4s

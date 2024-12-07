a8a74362fe1d421cf01fe7e092f6ad58c1775fa0
Jan Schormann <jan.schormann@rechengil.de>
2024-12-07 19:25:13 +0100

    2023-03a

--- 2023/03/03a.py
+++ 2023/03/03a.py
@@ -1,4 +1,6 @@
 import fileinput
+import re
+import string
 
 lines = [line.strip() for line in fileinput.input()]
 width = len(lines[0]) + 1
@@ -7,3 +9,29 @@ text = buffer_line + '.'.join(lines) + buffer_line
 
 if width < 20:
     print(text)
+
+numbers_re = re.compile(r'\d+')
+
+sum = 0
+for match in numbers_re.finditer(text):
+    start, end = match.span(0)
+    start, end = start-1, end+1
+    vicinity = (text[start-width:end-width] + '\n'
+                + text[start:end] + '\n'
+                + text[start+width:end+width] + '\n')
+
+    if width < 20:
+        print(vicinity)
+
+    stripped = vicinity.strip(".\n" + string.digits)
+    if stripped:
+        if width < 20:
+            print(f"{stripped=}")
+        sum += int(match.group(0))
+        if width < 20:
+            print(vicinity)
+
+
+
+print(f"{sum=}")
+# 525911 0.043s

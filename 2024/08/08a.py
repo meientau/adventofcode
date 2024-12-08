ca1792bff50b206065dcff58832ef75f0045042d
Jan Schormann <jan.schormann@rechengil.de>
2024-12-08 06:46:34 +0100

    2024-08a

--- 2024/08/08a.py
+++ 2024/08/08a.py
@@ -13,6 +13,7 @@ locations = defaultdict(list)
 for v, line in enumerate(input()):
     line = line.strip()
     for u, char in enumerate(line):
+        bottom_right = Point(max(bottom_right.u, u), max(bottom_right.v, v))
         if char != '.':
             locations[char].append(Point(u, v))
 
@@ -22,5 +23,16 @@ if len(locations) < 5:
 antinodes = set()
 for frequency, antennas in locations.items():
     for a, b in permutations(antennas, 2):
+        x = Point(2*b.u - a.u, 2*b.v - a.v)
+
         if len(locations) < 5:
-            print(f"{frequency=} {a=} {b=}")
+            print(f"{frequency=} {a=} {b=} {x=}")
+
+        if still_inside(x):
+            antinodes.add(x)
+
+if len(locations) < 5:
+    pprint(antinodes)
+print(len(antinodes))
+
+# 364 0.080s

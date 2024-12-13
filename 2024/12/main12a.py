b7a9faacfdd7af801d82a95ff5780f432be4bf48
Jan Schormann <jan.schormann@brainlab.com>
2024-12-13 14:21:15 +0100

    2024-12 has area

--- 2024/12/main12a.py
+++ 2024/12/main12a.py
@@ -1,2 +1,19 @@
+from collections import namedtuple
+
+Point = namedtuple("Point", ["u", "v"])
+
+class Field:
+    def __init__(self, u, v, crop):
+        self.entry = Point(u, v)
+        self.crop = crop
+        
+    def area(self):
+        return 1
+
 def find_all_fields(lines):
-    return [1]
+    all_fields = list()
+    for v, line in enumerate(lines):
+        for u, crop in enumerate(line.strip()):
+            all_fields.append(Field(u, v, crop))
+            
+    return all_fields
--- 2024/12/test_main12a.py
+++ 2024/12/test_main12a.py
@@ -3,6 +3,12 @@ import unittest
 # This is the first day where I found I need a real unit test to get it right.
 
 class Main12a_Test(unittest.TestCase):
-    def test_single(self):
+    def test_single_field_exists(self):
         fields = main12a.find_all_fields(["a\n"])
         self.assertEqual(1, len(fields))
+
+    def test_single_field_has_an_area(self):
+        fields = main12a.find_all_fields(["a\n"])
+        self.assertEqual(1, fields[0].area())
+
+    

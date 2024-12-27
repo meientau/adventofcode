1448491998b90ac744ab2409b6eebcb1d874c38a
Jan Schormann <jan.schormann@rechengil.de>
2024-12-27 18:23:10 +0100

    2024/15 yay!

--- 2024/15/main15b.py
+++ 2024/15/main15b.py
@@ -21,10 +21,16 @@ class Thing:
             self.other = twin
         else:
             self.twin = False
-            self.other = self.__class__(Point(p.u+1, p.v), twin=True)
+            self.other = self.__class__(Point(p.u+1, p.v), twin=self)
 
     def getshowlabel(self):
         return self.__class__.label
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return f"{self.label}-{self.position}"
 
 
 class Space:
@@ -37,7 +43,7 @@ class Space:
 class Wall(Thing):
     label = '#'
 
-    def move_please(self, direction):
+    def move_please(self, *_):
         return False
 
     def gps_coordinates(self):
@@ -48,25 +54,49 @@ class Crate(Thing):
     label = 'O'
     showlabel = '[]'
 
-    def move_please(self, direction):
+    def move_please(self, direction, movers=None):
+        if movers is None:
+            movers = set()
+
+        if self in movers:
+            return movers
+
+        movers.add(self)
+
         new_position = Point(self.position.u + direction.u,
-                             self.position.v + direction.v,)
+                             self.position.v + direction.v)
 
+        to_check = set()
         if new_position in things:
-            neighbour = things[new_position]
-            ok = neighbour.move_please(direction)
-        else:
-            ok = True
+            to_check.add(things[new_position])
 
-        if ok:
-            del things[self.position]
-            things[new_position] = self
-            self.position = new_position
-            return True
+        if self.other:
+            to_check.add(self.other)
 
-        return False
+        for other in to_check:
+            ok = other.move_please(direction, movers)
+            if ok is False: return False
+            movers |= ok
+
+        return movers
+    
+    # If I moved each thing one-by-one on the map, I would need to be careful
+    # about order I move them so they don't erase each other.
+    # It's easier to first collect all things that move, then remove them from the map,
+    # then put them back on at the new position
+    def jump(self):
+        del things[self.position]
+
+    def land(self, direction):
+        new_position = Point(self.position.u + direction.u,
+                             self.position.v + direction.v)
+        things[new_position] = self
+        self.position = new_position
 
     def gps_coordinates(self):
+        if self.twin: # each box counts only once
+            return 0
+
         return self.position.u + 100 * self.position.v
 
     def getshowlabel(self):
@@ -76,14 +106,32 @@ class Crate(Thing):
 robot = None
 class Robot(Crate):
     label = '@'
+    showlabel = label
 
-    def __init__(self, p, twin=False):
-        Crate.__init__(self, p, twin=True)
+    def __init__(self, p, *_):
+        things[p] = self
+        self.position = p
+        self.twin = False
+        self.other = None
         global robot
         robot = self
 
     def gps_coordinates(self):
         return 0
+
+    def move_a_lot(self, line):
+        for char in line:
+            dir = directions[char]
+            movers = self.move_please(dir)
+
+            if not movers: continue
+
+            for m in movers:
+                m.jump()
+
+            for m in movers:
+                m.land(dir)
+
 
 
 classes[Space.label] = Space
@@ -99,11 +147,6 @@ def read_line_into_field(v, line):
 
     global bottom_right
     bottom_right = Point(2*u+1, v)
-
-
-def move_robot(line):
-    for char in line:
-        robot.move_please(directions[char])
 
 
 def show_field():
@@ -132,10 +175,10 @@ for v, line in enumerate(fileinput.input()):
     if still_defining_field:
         read_line_into_field(v, line)
     else:
-        move_robot(line)
+        robot.move_a_lot(line)
 
 print()
 show_field()
 total = sum(thing.gps_coordinates() for thing in things.values())
 print(f"{total=}")
-# total=1414416  0.082s
+# total=1386070   0.032s

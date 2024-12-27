e25bec40a86d9fb802793294ba36961df840b0f3
Jan Schormann <jan.schormann@rechengil.de>
2024-12-27 17:42:00 +0100

    2024-25a done, now I need 19 more stars to see part 2

--- 2024/25/main25a.py
+++ 2024/25/main25a.py
@@ -1,14 +1,15 @@
 import fileinput
+import itertools
 
-keys = list()
-locks = list()
+keys = set()
+locks = set()
 current = 0
 this_is = None
 for line in fileinput.input():
     line = line.strip()
     if not line:
         if current:
-            this_is.append(current)
+            this_is.add(current)
         current = 0
         this_is = None
         continue
@@ -38,16 +39,26 @@ if len(keys) < 10:
 
     print()
 
+def to_pins(v):
+    pins = f"{v:030b}".replace('0', '.').replace('1', '#')
+    return [''.join(s) for s in itertools.batched(pins, 5)]
+
+def show_schematic(key, lock):
+    print("  key   lock  k&l")
+    print(f"  ..... ##### .....   {'' if (key&lock) else 'OK!'}")
+    for a, b, c in zip(to_pins(key), to_pins(lock), to_pins(key&lock)):
+        print(f"  {a} {b} {c}")
+    print()
+
 
 total = 0
-for key in keys:
-    for lock in locks:
+for lock in locks:
+    for key in keys:
         if len(keys) < 10:
-            print(f"key:     {key:030b}")
-            print(f"lock:    {lock:030b}")
-            print(f"overlap: {lock&key:030b}")
+            show_schematic(key, lock)
         total += int(not(key&lock))
 
 print(f"{total=}")
 
-# total=3063 0.015s wrong
+# total=3063 0.015s wrong too low: forgot to add extra newline to end of input
+# total=3065 0.017s

5bac831501959a7a43f6a2478325c2520ede384f
Jan Schormann <jan.schormann@rechengil.de>
2025-01-04 17:03:55 +0100

    adds bxl dummy

---
+++ 2024/17/elf_computer.py
@@ -0,0 +1,11 @@
+class Operation():
+    def __init__(self):
+        self.op = 1
+
+class ElfComputer():
+    def __init__(self):
+        self.bxl = Operation()
+        self.b = 0
+
+    def run(self, program):
+        self.b = 17

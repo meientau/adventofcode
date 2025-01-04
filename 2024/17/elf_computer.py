ada95ac8079754842514cd2aae5878868ca16987
Jan Schormann <jan.schormann@rechengil.de>
2025-01-04 17:07:37 +0100

    bxl full

--- 2024/17/elf_computer.py
+++ 2024/17/elf_computer.py
@@ -1,11 +1,19 @@
+import itertools
+
 class Operation():
-    def __init__(self):
+    def __init__(self, ops):
         self.op = 1
+        ops[self.op] = self
+
+    def execute(self, arg, cpu):
+        cpu.b ^= arg
 
 class ElfComputer():
     def __init__(self):
-        self.bxl = Operation()
+        self.ops = dict()
+        self.bxl = Operation(self.ops)
         self.b = 0
 
     def run(self, program):
-        self.b = 17
+        for instruction, argument in itertools.batched(program, 2):
+            self.ops[instruction].execute(argument, self)

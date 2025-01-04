c21dd2c9841bdc81568729d9ae57277c2afa540e
Jan Schormann <jan.schormann@rechengil.de>
2025-01-04 14:00:50 +0100

    2024-19a

---
+++ 2024/19/main19a.py
@@ -0,0 +1,32 @@
+import fileinput
+
+
+def is_possible(line):
+    if not line:
+        return True
+
+    for prefix in available:
+        if line.startswith(prefix):
+            if len(available) < 20:
+                print(f"line '{line}' starts with '{prefix}'")
+            if is_possible(line[len(prefix):]):
+                return True
+    return False
+
+
+available = None
+possible = 0
+for line in fileinput.input():
+    line = line.strip()
+
+    if not line:
+        continue
+
+    if not available:
+        available = [t.strip(', ') for t in line.split()]
+        continue
+
+    possible += is_possible(line)
+
+print(possible)
+# 265  0.148s
